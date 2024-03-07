27943b9ab94825af902971fdc4da2f4336ab6af0
Joanna Rodriguez <140280268+joannarodriguez134@users.noreply.github.com>
2024-03-07 15:36:28 +0000

    Saving current changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,8 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[ show edit update destroy ]
+  before_action :is_an_authorized_user, only: [:destroy, :create]
+
+  # make comments destroyable if you are not the currently signed in user
 
   # GET /comments or /comments.json
   def index
@@ -61,6 +64,13 @@ class CommentsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
       @comment = Comment.find(params[:id])
+    end
+
+    def is_an_authorized_user
+      @photo = Photo.find(params.fetch(:comment).fetch(:photo_id))
+      if current_user != @photo.owner && @photo.owner.private? && !current_user.leaders.include?(@photo.owner)
+        redirect_back fallback_location: root_url, alert: "Not authorized"
+      end
     end
 
     # Only allow a list of trusted parameters through.
