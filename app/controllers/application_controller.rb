3025b7b083aa60534b0ce2e31d097ae0fcb161ea
Joanna Rodriguez <140280268+joannarodriguez134@users.noreply.github.com>
2024-03-07 22:04:56 +0000

    Added pundit gem and started working on photo policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
   before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  
 
   protected
 
@@ -9,4 +12,14 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :private, :name, :bio, :website, :avatar_image])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :private, :name, :bio, :website, :avatar_image])
   end
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_back(fallback_location: root_path)
+  end
+  
 end
---
+++ app/policies/photo_policy.rb
@@ -0,0 +1,15 @@
+class PhotoPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+
+  end
+
+  def show?
+    true
+  end
+
+
+end
